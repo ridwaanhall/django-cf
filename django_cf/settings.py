424937f8539246752b1fb7535cbbffe7cc3368c8
Ridwan Halim <91854659+ridwaanhall@users.noreply.github.com>
2025-07-22 00:47:44 +0700

    Update database engine to use dummy backend in settings.py

--- django_cf/settings.py
+++ django_cf/settings.py
@@ -74,8 +74,7 @@ WSGI_APPLICATION = 'django_cf.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.dummy'
     }
 }
 
